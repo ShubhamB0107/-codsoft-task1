9ab0153a10d41e32841134d0674da9e3d8f79805
ShubhamB0107 <168751308+ShubhamB0107@users.noreply.github.com>
2024-08-30 12:30:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const msg_btn = document.querySelector('#send-msg-btn');
+// const dev_img = document.querySelector('#dev-img');
+
+
+// dev_img.addEventListener('click', ()=>{
+//     dev_img.innerHTML = '<img src="./imgs/dev5.jpg" alt="img">'
+// })
+// console.log(dev_img.innerHTML);
+msg_btn.addEventListener('click',()=>{
+    msg_btn.innerText = 'Message Sent!!'
+})
